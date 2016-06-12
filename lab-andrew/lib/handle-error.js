dd39e02fb3a0a0cd03749888bfd8dcbe435eaa7a
Andrew Watson <andrewpeterwatson@gmail.com>
2016-06-12 11:16:08 -0700

    added error handling

---
+++ lab-andrew/lib/handle-error.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+const httpErrors = require('http-errors');
+
+module.exports = function(err, req, res, next) {
+  console.error(err.message);
+  if(err.status && err.name) {
+    res.status(err.status).send(err.name);
+    next();
+    return;
+  }
+  err = httpErrors(500, err.message);
+  res.status(err.status).send(err.name);
+};
